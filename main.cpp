755a4c5f92ffe1a1e72da0d8aae20a070a6e8f45
Avinash Raj Hans <avinashrajhans001@gmail.com>
2025-03-06 19:20:26 +0530

    Made a change in master

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "I am learning Git\n";
     std::cout << "Git is fun so far\n";
     std::cout << "Added this line a fourth time to master\n";
+    std::cout << "Added a second line here in master\n";
     return 0;
 }
