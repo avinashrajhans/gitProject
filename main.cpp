ab961086a0a291fa36f8809beeb7ee428378f8bf
Avinash Raj Hans <avinashrajhans001@gmail.com>
2025-03-06 19:17:48 +0530

    Trying to remove the obj file

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main()
 {
     std::cout << "I am learning Git\n";
     std::cout << "Git is fun so far\n";
-    std::cout << "Added this line a second time to master\n";
+    std::cout << "Added this line a fourth time to master\n";
     return 0;
 }
